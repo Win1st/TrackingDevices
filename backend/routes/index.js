930328d0deb8dd4794a44be1039b79d3568bf108
Wachirapol Klinkasorn <wachirapol2545@gmail.com>
2024-11-02 22:13:01 +0700

    Update index.js

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -3,5 +3,15 @@ const pool = require("../config");
 
 router = express.Router();
 
+router.get("/", async function (req, res, next) {
+    try {
+      let sql = 'SELECT * FROM accounts'
+      const [rows, fields] = await pool.query(sql);
+      return res.json(rows);
+    } catch (err) {
+      return res.status(500).json(err)
+    }
+  });
+
 
 exports.router = router;
