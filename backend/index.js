c23c8b5112a2cb2348c8e2b4d49e7b1cef40ba76
Wachirapol Klinkasorn <wachirapol2545@gmail.com>
2024-11-02 18:18:46 +0700

    backEnd

---
+++ backend/index.js
@@ -0,0 +1,30 @@
+const express = require("express")
+const cookieSession = require('cookie-session');
+const path = require("path");
+
+const app = express();
+const cors = require('cors')
+app.use(cors())
+
+// Statics
+app.use('/static', express.static(path.join(__dirname, 'static')));
+app.use(express.json()) // for parsing application/json
+app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+
+
+app.use(cookieSession({
+  name: 'session',
+  keys: ['key1', 'key2'],
+  maxAge:  3600 * 1000 // 1hr
+}));
+
+// routers
+const indexRouter = require('./routes/index')
+
+app.use(indexRouter.router)
+
+
+
+app.listen(3000, () => {
+  console.log(`Example app listening at http://localhost:3000`)
+})
---
+++ backend/routes/index.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const pool = require("../config");
+
+router = express.Router();
+
+
+exports.router = router;
