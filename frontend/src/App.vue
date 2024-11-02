f2c658cb259267ed8c62b34f61d6519c7a2b2528
Wachirapol Klinkasorn <wachirapol2545@gmail.com>
2024-11-02 23:57:54 +0700

    Update App.vue

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,20 +1,57 @@
 <template>
   <div id="app">
     <!-- nav bar -->
-    <nav
-      class="navbar navbar-light sticky-top border-bottom bg-dark"
-      style="height: 10vh"
-    ></nav>
+    <nav class="navbar navbar-light sticky-top border-bottom bg-dark" style="height: 10vh;">
+            <div class="container-fluid">
+
+                  <div class="d-flex">
+                    <!-- logo -->
+                    
+                  </div>
+                    <template v-if="this.$cookies.isKey('account')">
+                        <div :class="center" >
+                            <router-link to="/user/profile" class="d-flex" :class="center" style="text-decoration: none;">
+                              <div class="bi bi-person text-light mx-3" style="font-size: 2rem;"></div>
+                              <div class=' fw-bold fs-8 mx-1 text-white' :class="center">{{this.$cookies.get('account').username}}</div>
+                            </router-link>
+                            <button type="button" class="btn btn-danger btn-sm mx-2" @click.stop="logout()">Logout</button>
+                        </div>
+                    </template>
+                    <template v-else>
+                      <router-link to="/user/login" style="text-decoration: none;">
+                        <div :class="center">
+                            <div class="bi bi-person text-light mx-3" style="font-size: 2rem;"></div>
+                            <div class=' fw-bold fs-8 mx-1 text-white' :class="center">เข้าสู่ระบบ/ลงทะเบียน</div>
+                        </div>
+                      </router-link>
+                    </template>
+            </div>
+    </nav>
     <router-view :key="$route.fullPath" />
-    <footer class="bg-dark" style="height: 10vh"></footer>
+    <footer class="bg-dark" style="height: 10vh;"></footer>
   </div>
 </template>
+
 
 <script>
 export default {
   data() {
-    return {};
+    return {
+      center:{
+          'd-flex': true,
+          'justify-content-center':true,
+          'align-items-center':true
+      },
+    };
   },
-  methods: {},
+  methods: {
+    logout () {
+      this.$cookies.keys().forEach(cookie => this.$cookies.remove(cookie))
+      alert("Logout");
+      this.$router.push({path: '/user/login'})
+    },
+  },
 };
+
+        
 </script>
